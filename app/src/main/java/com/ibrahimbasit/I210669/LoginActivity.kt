bb6e83b084c2c12d363b798eb63508b73184fada
Muhammad Ibrahim Basit <53190064+IbrahimBasit5802@users.noreply.github.com>
2024-02-06 10:33:40 +0500

    login to main navigation

--- app/src/main/java/com/ibrahimbasit/I210669/LoginActivity.kt
+++ app/src/main/java/com/ibrahimbasit/I210669/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
@@ -20,6 +21,12 @@ class LoginActivity : AppCompatActivity() {
         val forgotPasswordLink : TextView = findViewById(R.id.forgotPasswordTextView);
         forgotPasswordLink.setOnClickListener {
             val intent = Intent(this, ForgotPasswordActivity::class.java)
+            startActivity(intent)
+        }
+
+        val loginButton : Button = findViewById(R.id.loginButton)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
 
