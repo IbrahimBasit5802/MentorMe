2b42ce9b01f6e7f3096d927960d401d4b7fda59f
Muhammad Ibrahim Basit <53190064+IbrahimBasit5802@users.noreply.github.com>
2024-02-06 10:32:30 +0500

    fragments created and nav bar navigation implemented between fragments

--- app/src/main/java/com/ibrahimbasit/I210669/MainActivity.kt
+++ app/src/main/java/com/ibrahimbasit/I210669/MainActivity.kt
@@ -2,13 +2,43 @@ package com.ibrahimbasit.I210669
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.ibrahimbasit.I210669.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
 
+        setContentView(binding.root)
 
-        // test commit
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavBar.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> {
+                    replaceFragment(HomeFragment())
+                }
+                R.id.search -> {
+                    replaceFragment(SearchFragment())
+                }
+                R.id.chat -> {
+                    replaceFragment(ChatFragment())
+                }
+                R.id.profile -> {
+                    replaceFragment(ProfileFragment())
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frame_layout, fragment)
+        transaction.commit()
     }
 }
