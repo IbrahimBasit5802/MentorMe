317202ac28b1db4a28e3773ef4add66a413dfaf3
Muhammad Ibrahim Basit <53190064+IbrahimBasit5802@users.noreply.github.com>
2024-02-08 14:57:22 +0500

    assets for call screen

---
+++ app/src/main/java/com/ibrahimbasit/I210669/CallScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.ibrahimbasit.I210669
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CallScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_call_screen_acitivty)
+    }
+}
