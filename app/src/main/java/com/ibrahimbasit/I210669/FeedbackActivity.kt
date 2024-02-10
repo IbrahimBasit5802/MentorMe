74264415339199125007d26773fa5b0c0ec2d37b
Muhammad Ibrahim Basit <53190064+IbrahimBasit5802@users.noreply.github.com>
2024-02-11 00:17:58 +0500

    update fonts for some screens

---
+++ app/src/main/java/com/ibrahimbasit/I210669/FeedbackActivity.kt
@@ -0,0 +1,11 @@
+package com.ibrahimbasit.I210669
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FeedbackActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feedback)
+    }
+}
