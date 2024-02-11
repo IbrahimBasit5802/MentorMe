6a76bdd1e0351b1278cca3dba8ffd617bc895e31
Muhammad Ibrahim Basit <53190064+IbrahimBasit5802@users.noreply.github.com>
2024-02-11 23:09:00 +0500

    espresso test case one tested and passed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,18 +37,22 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
 }
 
 dependencies {
     implementation("androidx.core:core-splashscreen:1.1.0-alpha02")
     implementation ("androidx.cardview:cardview:1.0.0")
     implementation ("de.hdodenhof:circleimageview:3.1.0")
+    implementation("androidx.test.espresso:espresso-intents:3.5.1")
+    androidTestImplementation ("androidx.navigation:navigation-testing:2.5.3")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
+    implementation("androidx.test.ext:junit-ktx:1.1.5")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation ("androidx.test:runner:1.5.1")
 }
---
+++ app/src/test/java/com/ibrahimbasit/I210669/ActivityNavigationTest.kt
@@ -0,0 +1,38 @@
+package com.ibrahimbasit.I210669
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ActivityNavigationTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(LoginActivity::class.java)
+
+    @Before
+    fun setup() {
+        Intents.init()
+    }
+
+    @After
+    fun cleanup() {
+        Intents.release()
+    }
+
+    @Test
+    fun testNavigationToSecondActivity() {
+        onView(withId(R.id.loginButton)).perform(click())
+        intended(hasComponent(MainActivity::class.java.name))
+    }
+}
