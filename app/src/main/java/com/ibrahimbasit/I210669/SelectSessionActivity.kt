752e2acd12f8171237ee6e5294ec3982de071b9a
Muhammad Ibrahim Basit <53190064+IbrahimBasit5802@users.noreply.github.com>
2024-02-13 00:24:26 +0500

    selevct session screen half done

--- app/src/main/java/com/ibrahimbasit/I210669/SelectSessionActivity.kt
+++ app/src/main/java/com/ibrahimbasit/I210669/SelectSessionActivity.kt
@@ -3,15 +3,19 @@ package com.ibrahimbasit.I210669//package com.ibrahimbasit.I210669
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.GridLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 
 class SelectSessionActivity : AppCompatActivity() {
 
     private var monthHeader: TextView? = null
     private var calendarGrid: GridLayout? = null
+    private lateinit var buttonsList: List<Button>
+    private var selectedButton: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_session)
@@ -21,6 +25,20 @@ class SelectSessionActivity : AppCompatActivity() {
         val backButton : View = findViewById(R.id.backButton)
         backButton.setOnClickListener {
             finish()
+        }
+
+        val timeSlotButton : Button = findViewById(R.id.timeSlot1)
+        val timeSlotButton2 : Button = findViewById(R.id.timeSlot2)
+        val timeSlotButton3 : Button = findViewById(R.id.timeSlot3)
+        val timeSlotButton4 : Button = findViewById(R.id.timeSlot4)
+
+
+        setSelectedButton(timeSlotButton2)
+        buttonsList = (listOf(timeSlotButton, timeSlotButton2, timeSlotButton3, timeSlotButton4))
+        buttonsList.forEach { button ->
+            button.setOnClickListener { clickedButton ->
+                setSelectedButton(clickedButton as Button)
+            }
         }
 
         // Set the month and year
@@ -43,5 +61,19 @@ class SelectSessionActivity : AppCompatActivity() {
                 calendarGrid?.addView(dayCell)
             }
         }
+    }
+
+    private fun setSelectedButton(button: Button) {
+        // Change background and text color of the clicked button
+        selectedButton?.let {
+            it.backgroundTintList = ContextCompat.getColorStateList(this, R.color.timeSloeUnselectedColor)
+            it.setTextColor(ContextCompat.getColor(this, R.color.secondaryTextColor))
+        }
+
+        button.backgroundTintList = ContextCompat.getColorStateList(this, R.color.timeSlotColor)
+        button.setTextColor(ContextCompat.getColor(this, R.color.primaryTextColor))
+
+        // Keep reference to the selected button
+        selectedButton = button
     }
 }
